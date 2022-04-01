627f409213b57cdcbcc5e4e3b774853b34d5367f
Telma Medina <misolocks@gmail.com>
2022-04-01 19:40:22 +0100

    Add initial solutions

---
+++ javascript/8-kyu/multiply/main.js
@@ -0,0 +1,11 @@
+/************************************************************ 
+ * Multiply
+ * 
+ * Task:
+ * This code does not execute properly. Try to figure out why.
+ * 
+*************************************************************/
+
+function multiply(a, b){
+    return a * b
+  }
