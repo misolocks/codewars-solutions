211535fb7a5570618e5e9a37d738629e247602ea
Telma Medina <misolocks@gmail.com>
2022-04-12 23:52:27 +0100

    Add MakeUpperCase solution

---
+++ javascript/8-kyu/make-upper-case/main.js
@@ -0,0 +1,11 @@
+/************************************************************ 
+MakeUpperCase
+
+Task:
+Write a function which converts the input string to uppercase.
+*************************************************************/
+
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase()
+}
